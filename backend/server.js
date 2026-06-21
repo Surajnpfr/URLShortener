6362e2a0c2cda0fa705c010fe3be451258a282c5
Suraj <nepalsuraj178@gmail.com>
2026-06-21 19:39:38 +0545

    add /favicon.ico GET route in backend

--- backend/server.js
+++ backend/server.js
@@ -48,6 +48,14 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/favicon.ico", (req, res) => {
+  res.status(200).type("image/svg+xml").send(`
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#eab308">
+      <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>
+    </svg>
+  `);
+});
+
 // API: Get all URLs
 app.get('/api/urls', async (req, res) => {
   try {
